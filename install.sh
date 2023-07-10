8f98d55da3381ff574c75bd2ca068f9c4f13b05b
Fábio Rosa <quintellarosa@gmail.com>
2023-07-10 10:44:18 -0400

    First commit

---
+++ install.sh
@@ -0,0 +1,7 @@
+export VERSION=1.25
+export OS=CentOS_8
+curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable.repo https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/devel:kubic:libcontainers:stable.repo
+curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.repo https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/devel:kubic:libcontainers:stable:cri-o:$VERSION.repo
+sudo dnf install crio -y
+sudo systemctl status crio
+sudo systemctl start crio
